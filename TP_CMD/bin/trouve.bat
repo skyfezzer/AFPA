67886eaec90a5cf9f428818434bbeb855d5059a5
Alexis RAGOT <alexisragot77@gmail.com>
2021-11-02 10:54:40 +0100

    fix: incrementation qui n'était pas dans la boucle

--- TP_CMD/bin/trouve.bat
+++ TP_CMD/bin/trouve.bat
@@ -3,9 +3,9 @@
 
 set /a secretNumber="%RANDOM% %%9+1"
 set nbTour = 0
-set /a nbTour+=1
 
 :ASKNUMBER
+set /a nbTour+=1
 set /p nbr=Trouvez le nombre entre 0 et 9..
 IF %nbr% EQU %secretNumber% GOTO :WIN
 IF %nbr% GTR %secretNumber% ( set result=moins) ELSE set result=plus
