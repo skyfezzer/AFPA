658340ee95b8336cac73615891c674176ede3d8c
Alexis RAGOT <alexisragot77@gmail.com>
2021-11-04 17:07:55 +0100

    Ajout de l'exercice 3 v2.2 TP_JAVA

---
+++ TP_JAVA/03/Version1.java
@@ -0,0 +1,48 @@
+import java.util.Scanner;
+import java.util.Arrays;
+public class Version1 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String nom, prenom, adresse, fnte;
+        char rep;
+        do{
+            // INPUTS
+            System.out.print("Nom du cavalier\t: ");
+            nom = sc.nextLine();
+
+            System.out.print("Prénom du cavalier\t: ");
+            prenom = sc.nextLine();
+
+            System.out.print("Adresse du cavalier\t: ");
+            adresse = sc.nextLine();
+
+            System.out.print("Numero de carte FNTE\t: ");
+            fnte = sc.nextLine();
+
+            System.out.print("Vos 3 chevaux preferes (entrer 1 par 1)\t: ");
+            String[] chevauxPref = {sc.nextLine(),sc.nextLine(),sc.nextLine()};
+
+            // PRESENTATION 1
+            System.out.printf("\n\nNom du cavalier \t: %s\n",nom);
+            System.out.printf("Prenom du cavalier \t: %s\n",prenom);
+            System.out.printf("Adresse du cavalier \t: %s\n",adresse);
+            System.out.printf("Chevaux preferes \t: %s\n",chevauxPref[0]);
+            System.out.printf("\t\t\t: %s\n",chevauxPref[1]);
+            System.out.printf("\t\t\t: %s\n",chevauxPref[2]);
+            System.out.printf("No carte FNTE \t: %s\n",fnte);
+
+            // PRESENTATION 2
+            System.out.println("\n\n");
+            System.out.println("Cavalier no "+ fnte);
+            System.out.println("\t" + nom + " " + prenom);
+            System.out.println("\t"+adresse);
+            System.out.println("Chevaux ; " + Arrays.toString(chevauxPref));
+
+            // MESSAGE DE FIN
+            System.out.print("\n\nVoulez-vous continuer (O/N) ?");
+            rep = sc.nextLine().charAt(0);
+        }while(rep != 'o');
+
+        sc.close();
+    }
+}
