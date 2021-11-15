98d12344256e4147c7759f885e1ee4f1fe8b8fa4
Alexis RAGOT <alexisragot77@gmail.com>
2021-11-15 20:06:15 +0100

    Update Exo4_03.java

--- TP_JAVA/04_darmangeat/src/darman/part4/Exo4_03.java
+++ TP_JAVA/04_darmangeat/src/darman/part4/Exo4_03.java
@@ -18,7 +18,7 @@ public class Exo4_03 {
 		secondes = sc.nextInt();
 		sc.nextLine();
 		sc.close();
-		System.out.printf("Dans une minute, il sera %.0fh%.0f:%.0f ", 
+		System.out.printf("Dans une seconde, il sera %.0fh%.0f:%.0f ", 
 				(heures+(Math.floor(((minutes+Math.floor((secondes+1)/60)))/60)))%24f, 
 				(minutes+Math.floor((secondes+1)/60))%60f, 
 				(secondes+1)%60f);
